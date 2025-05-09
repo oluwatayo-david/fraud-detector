1c82dc136f45ed3ed411bbb3f686976c136e028a
David Oluwatayo <oluwatayodavid273@gmail.com>
2025-05-09 13:38:42 +0100

    changes made

--- components/ui/canvas-reveal-effect.tsx
+++ components/ui/canvas-reveal-effect.tsx
@@ -25,27 +25,27 @@ export const CanvasRevealEffect = ({
 }) => {
     return (
         <div className={cn("h-full relative bg-white w-full", containerClassName)}>
-    <div className="h-full w-full">
-    <DotMatrix
-        colors={colors ?? [[0, 255, 255]]}
-    dotSize={dotSize ?? 3}
-    opacities={
-        opacities ?? [0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1]
-}
-    shader={`
+            <div className="h-full w-full">
+                <DotMatrix
+                    colors={colors ?? [[0, 255, 255]]}
+                    dotSize={dotSize ?? 3}
+                    opacities={
+                        opacities ?? [0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1]
+                    }
+                    shader={`
               float animation_speed_factor = ${animationSpeed.toFixed(1)};
               float intro_offset = distance(u_resolution / 2.0 / u_total_size, st2) * 0.01 + (random(st2) * 0.15);
               opacity *= step(intro_offset, u_time * animation_speed_factor);
               opacity *= clamp((1.0 - step(intro_offset + 0.1, u_time * animation_speed_factor)) * 1.25, 1.0, 1.25);
             `}
-    center={["x", "y"]}
-    />
-    </div>
-    {showGradient && (
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-[84%]" />
-    )}
-    </div>
-);
+                    center={["x", "y"]}
+                />
+            </div>
+            {showGradient && (
+                <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-[84%]" />
+            )}
+        </div>
+    );
 };
 
 interface DotMatrixProps {
@@ -141,15 +141,15 @@ const DotMatrix: React.FC<DotMatrixProps> = ({
         void main() {
             vec2 st = fragCoord.xy;
             ${
-            center.includes("x")
-                ? "st.x -= abs(floor((mod(u_resolution.x, u_total_size) - u_dot_size) * 0.5));"
-                : ""
-        }
+                center.includes("x")
+                    ? "st.x -= abs(floor((mod(u_resolution.x, u_total_size) - u_dot_size) * 0.5));"
+                    : ""
+            }
             ${
-            center.includes("y")
-                ? "st.y -= abs(floor((mod(u_resolution.y, u_total_size) - u_dot_size) * 0.5));"
-                : ""
-        }
+                center.includes("y")
+                    ? "st.y -= abs(floor((mod(u_resolution.y, u_total_size) - u_dot_size) * 0.5));"
+                    : ""
+            }
       float opacity = step(0.0, st.x);
       opacity *= step(0.0, st.y);
 
@@ -169,10 +169,10 @@ const DotMatrix: React.FC<DotMatrixProps> = ({
       fragColor = vec4(color, opacity);
       fragColor.rgb *= fragColor.a;
         }`}
-    uniforms={uniforms}
-    maxFps={60}
-    />
-);
+            uniforms={uniforms}
+            maxFps={60}
+        />
+    );
 };
 
 type Uniforms = {
@@ -192,7 +192,7 @@ const ShaderMaterial = ({
     uniforms: Uniforms;
 }) => {
     const { size } = useThree();
-    const ref = useRef<THREE.Mesh>();
+    const ref = useRef<THREE.Mesh>(null);
     let lastFrameTime = 0;
 
     useFrame(({ clock }) => {
@@ -283,18 +283,18 @@ const ShaderMaterial = ({
 
     return (
         <mesh ref={ref as any}>
-        <planeGeometry args={[2, 2]} />
-    <primitive object={material} attach="material" />
+            <planeGeometry args={[2, 2]} />
+            <primitive object={material} attach="material" />
         </mesh>
-);
+    );
 };
 
 const Shader: React.FC<ShaderProps> = ({ source, uniforms, maxFps = 60 }) => {
     return (
         <Canvas className="absolute inset-0  h-full w-full">
-        <ShaderMaterial source={source} uniforms={uniforms} maxFps={maxFps} />
-    </Canvas>
-);
+            <ShaderMaterial source={source} uniforms={uniforms} maxFps={maxFps} />
+        </Canvas>
+    );
 };
 interface ShaderProps {
     source: string;
